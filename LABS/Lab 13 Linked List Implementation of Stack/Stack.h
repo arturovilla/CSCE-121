f45f4c5e116276eadfaeb720823eb0a672e4f616
arturo villalobos <https://github.com/arturovilla/CSCE-121.git>
2020-05-07 19:12:37 -0500

    Lab 13 Linked List Implementation of Stack

---
+++ LABS/Lab 13 Linked List Implementation of Stack/Stack.h
@@ -0,0 +1,52 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <iostream>
+#include "LinkedList.h"
+
+template <typename T>
+class Stack {
+    LinkedList<T> list;
+    
+    public:
+    bool empty();
+    T& peek();
+    void pop();
+    void push(const T&);
+    
+    friend std::ostream& operator<<(std::ostream& os, const Stack& stack) {
+        os << stack.list;
+        return os;
+    }
+};
+
+template <typename T>
+bool Stack<T>::empty()
+{
+    if(list.length() == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+T& Stack<T>::peek()
+{
+    
+     return list.front();
+}
+
+template <typename T>
+void Stack<T>::pop()
+{
+    list.pop_front();
+}
+
+template <typename T>
+void Stack<T>::push(const T& elem)
+{
+     list.push_front(elem);
+}
+
+#endif
